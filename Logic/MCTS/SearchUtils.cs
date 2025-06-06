d2d6a797770395b6f4a9a5bfa68f306c35742aa7
Liam McGuire <12057628+liamt19@users.noreply.github.com>
2025-06-06 17:34:26 -0400

    Slightly adjust temperature by depth (#8)
    bench: 6764894

--- Logic/MCTS/SearchUtils.cs
+++ Logic/MCTS/SearchUtils.cs
@@ -30,10 +30,14 @@ public static unsafe class SearchUtils
         return float.Exp(0.5f * float.Log10(Math.Max(node.Visits, 1)));
     }
 
-    public static float GetTemperatureAdjustment(uint depth, float q)
+    public static float GetTemperatureAdjustment(int depth, float q)
     {
         var value = (q - Math.Min(q, TemperatureQInc)) / (1.0f - TemperatureQInc);
-        return 1.0f + value * TemperatureScale;
+
+        //  sin(x * pi / 2) / 8
+        var dCoef = ((2 - (depth % 4)) * (depth % 2)) / 8.0f;
+
+        return 1.0f + value * TemperatureScale + dCoef;
     }
 
     public static float PolicyForMove(Position pos, Move move)

542b63007fb1396ba617028ad92f64df0279912b
Liam McGuire <liam.t.mcguire@gmail.com>
2025-06-14 18:38:52 -0400

    call it there bench: 17976321

--- Logic/Search/SearchOptions.cs
+++ Logic/Search/SearchOptions.cs
@@ -18,15 +18,15 @@
         //public static int SEEValueRook = 535;
         //public static int SEEValueQueen = 970;
 
-        public static float ExplTau = 0.6442f;
-        public static float CPuctVisitScale = 4011.9372f;
-        public static float CPuctBaseRoot = 0.5173f;
-        public static float CPuctBase = 0.2805f;
-        public static float PSTQInc = 0.7906f;
-        public static float PSTQScale = 0.2559f;
-        public static float PSTNumer = 2.2811f;
-        public static float PSTPow = 1.4742f;
-        public static float PSTOffset = 0.1489f;
-        public static float PSTSinDiv = 25.4630f;
+        public static float ExplTau = 0.6493f;
+        public static float CPuctVisitScale = 3999.7565f;
+        public static float CPuctBaseRoot = 0.5141f;
+        public static float CPuctBase = 0.2793f;
+        public static float PSTQInc = 0.7948f;
+        public static float PSTQScale = 0.2565f;
+        public static float PSTNumer = 2.2689f;
+        public static float PSTPow = 1.4886f;
+        public static float PSTOffset = 0.1502f;
+        public static float PSTSinDiv = 25.5294f;
     }
 }

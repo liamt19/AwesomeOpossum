c834eabf4467269c7b64cba6c0c2796cf8317365
Liam McGuire <liam.t.mcguire@gmail.com>
2025-06-22 20:55:23 -0400

    bench: 12497658

--- Logic/Search/SearchOptions.cs
+++ Logic/Search/SearchOptions.cs
@@ -13,9 +13,9 @@
 
 
         public static float ExplTau = 0.6493f;
-        public static float GiniBase = 0.5350f;
-        public static float GiniScale = 1.4000f;
-        public static float GiniMin = 2.0000f;
+        public static float GiniBase = 0.6700f;
+        public static float GiniScale = 1.5000f;
+        public static float GiniMin = 2.1500f;
         public static float CPuctVisitScale = 3999.7565f;
         public static float CPuctBaseRoot = 0.5141f;
         public static float CPuctBase = 0.2793f;

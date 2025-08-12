3aba3b388fef20e0e5951e795beecaaff6d877db
Liam McGuire <12057628+liamt19@users.noreply.github.com>
2025-08-12 18:46:36 -0400

    1536 HL value + fix bench UB (#33)
    bench: 10344003

--- src/AwesomeOpossum/Logic/Util/SIMDBindings.cs
+++ src/AwesomeOpossum/Logic/Util/SIMDBindings.cs
@@ -8,15 +8,17 @@ namespace AwesomeOpossum.Logic.Util
 {
     public static unsafe partial class SIMDBindings
     {
-        public static readonly bool HasBindings;
+        public static bool HasBindings;
 
-        private static readonly nint Handle;
+        private static nint Handle;
 
-        private static readonly IntPtr PolicyEvaluateAddr;
-        private static readonly IntPtr ValueEvaluateAddr;
+        private static IntPtr PolicyEvaluateAddr;
+        private static IntPtr ValueEvaluateAddr;
 
         public static unsafe delegate* unmanaged<short*, short*, int> PolicyEvaluateFn;
         public static unsafe delegate* unmanaged<short*, sbyte*, float*, float*, float*, float*, float, float> ValueEvaluateFn;
+
+        private const string MUTEX_NAME = "Global\\AP_SIMD_MUTEX";
 
 #if IsWindows
         private const string DEST_NAME = "SIMDBindings.dll";
@@ -24,31 +26,70 @@ namespace AwesomeOpossum.Logic.Util
         private const string DEST_NAME = "SIMDBindings.so";
 #endif
 
+        private static readonly string DLLResouceName;
+        private static readonly string AbsoluteDLLDestPath;
+
         static SIMDBindings()
+        {
+            DLLResouceName = $"AwesomeOpossum.{DEST_NAME}";
+            AbsoluteDLLDestPath = Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory), DEST_NAME);
+
+            TryLoadBindings(verbose: true);
+        }
+
+
+        public static void SetDefaultFuncPtrs()
         {
             HasBindings = false;
 
+            if (Handle != default)
+            {
+                NativeLibrary.Free(Handle);
+                Handle = default;
+            }
+
             PolicyEvaluateFn = (delegate* unmanaged<short*, short*, int>)(&PolicyNetwork.EvaluateImpl);
             ValueEvaluateFn = (delegate* unmanaged<short*, sbyte*, float*, float*, float*, float*, float, float>)(&ValueNetwork.EvaluateImpl);
+        }
+
+
+        public static void TryLoadBindings(bool verbose = false)
+        {
+            SetDefaultFuncPtrs();
 
             if (!IsOSPlatform(OSPlatform.Windows) && !IsOSPlatform(OSPlatform.Linux))
                 return;
 
-            string resName = $"AwesomeOpossum.{DEST_NAME}";
-            string absPath = Path.Combine(Path.GetDirectoryName(AppContext.BaseDirectory), DEST_NAME);
+            using (var mutex = new Mutex(false, MUTEX_NAME))
+            {
+                bool hasHandle = false;
+                try
+                {
+                    hasHandle = mutex.WaitOne(5000);
+                    ExtractEmbeddedLibrary(DLLResouceName, DEST_NAME);
+                }
+                finally
+                {
+                    if (hasHandle) 
+                        mutex.ReleaseMutex();
+                }
+            }
+
+            if (Handle != default)
+            {
+                NativeLibrary.Free(Handle);
+                Handle = default;
+            }
 
             try
             {
-                if (!ExtractEmbeddedLibrary(resName, DEST_NAME) && !File.Exists(absPath))
-                {
-                    return;
-                }
-
-                Handle = NativeLibrary.Load(absPath);
+                Handle = NativeLibrary.Load(AbsoluteDLLDestPath);
             }
             catch (Exception e)
             {
-                Log($"Failed loading SIMD Bindings! :( \n{e}");
+                if (verbose)
+                    Log($"Failed loading SIMD Bindings! :( \n{e}");
+
                 return;
             }
 
@@ -64,7 +105,9 @@ namespace AwesomeOpossum.Logic.Util
             }
             catch (Exception e)
             {
-                Log($"Failed to find an entry point! \n{e}");
+                if (verbose)
+                    Log($"Failed to find an entry point! \n{e}");
+
                 return;
             }
 
@@ -72,20 +115,20 @@ namespace AwesomeOpossum.Logic.Util
             ValueEvaluateFn = (delegate* unmanaged<short*, sbyte*, float*, float*, float*, float*, float, float>)ValueEvaluateAddr;
 
             HasBindings = true;
-            Log("Loaded SIMD Bindings!");
+
+            if (verbose)
+                Log("Loaded SIMD Bindings!");
         }
+
 
         private static bool ExtractEmbeddedLibrary(string resName, string fileName)
         {
             var asm = Assembly.GetExecutingAssembly();
-            Debug.WriteLine($"looking for {resName} in [{string.Join(", ", asm.GetManifestResourceNames())}]");
+            //Debug.WriteLine($"looking for {resName} in [{string.Join(", ", asm.GetManifestResourceNames())}]");
             using Stream stream = asm.GetManifestResourceStream(resName);
 
             if (stream == null)
-            {
-                //Log("Running without SIMD Bindings");
                 return false;
-            }
 
             string exePath = Path.GetDirectoryName(AppContext.BaseDirectory);
             string dllPath = Path.Combine(exePath, fileName);
@@ -98,6 +141,5 @@ namespace AwesomeOpossum.Logic.Util
 
             return true;
         }
-
     }
 }
